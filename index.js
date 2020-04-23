b529947b04eba789440d7c0f468e1f6121da193b
HajimeNoJosh <joshuah.m.martin@gmail.com>
2020-04-23 18:23:09 -0400

    generating the url

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const input = document.querySelector("input");
+  const inputMessage = document.querySelector("#message-input");
+  const encrypted = btoa(inputMessage.value);
 
-  console.log(input.value);
+  const inputLink = document.querySelector("#link-input");
+  inputLink.value = `${window.location}#${encrypted}`;
+  inputLink.select();
 });
